0aa1c8486e6c0c9b0d63bf30ff5da28664a36da4
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-15 15:18:59 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    time_now = Time.now
+    noon = Time.new(Time.now.year, 
+                        Time.now.month, 
+                        Time.now.day, 
+                        12, 0, 0)
+                        
+    if time_now < noon
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+
+end
