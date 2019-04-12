c6df9db95894df143b16297976382babfbec4ba7
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-04-12 14:51:23 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def call(env)
+    time = Time.now
+    resp = Rack::Response.new
+
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+end
