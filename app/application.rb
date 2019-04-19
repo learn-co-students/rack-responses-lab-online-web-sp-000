20356ea3e81513cf81e37f43122d2862f7422677
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-19 18:01:30 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,12 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.new
+    time = Time.now
 
-    resp.write "#{time.hour}\n"
-    resp.write "#{time.zone}\n"
+    resp.write "#{time}\n"
 
     if time.hour >= 12
-      resp.write "Good Afternoon!, or Morning, these tests aren't helpful..."
+      resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
     end
