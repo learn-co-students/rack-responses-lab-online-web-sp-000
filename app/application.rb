51cf1ab57090799b626e4da76707989dc7caf847
Jim Hough <teachlife@outlook.com>
2019-06-05 00:39:10 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    hour = Time.now.strftime('%H').to_i
+    if hour >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
+  end
+end
