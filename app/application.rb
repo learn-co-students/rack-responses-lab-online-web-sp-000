3faf2cb3cbfe29fe6da89ed39fa3f2abf22b31d7
Yingqi Chen <chanwk18@hotmail.com>
2019-06-08 21:00:19 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,15 @@
 class Application
 
-  def call
+  def call(env)
+
     resp=Rack::Response.new
     time=Time.now
-    if
 
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else resp.write "Good Afternoon!"
+    end
 
     resp.finish
   end
+end
