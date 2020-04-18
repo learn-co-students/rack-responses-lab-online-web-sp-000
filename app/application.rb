c9f7de6a50566006bb4406e13fca8e8c92678b67
joshuaAjack <Mynameisjoshuajack@gmail.com>
2020-04-17 22:05:56 -0700

    Done

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+     def call(env)
+     resp = Rack::Response.new
+        
+     time = Time.now.hour
+           
+     time >= 12 ? greeting = "Good Afternoon!" : greeting = "Good Morning!"
+          
+     resp.write greeting
+          
+     resp.finish
+     end 
+ end
