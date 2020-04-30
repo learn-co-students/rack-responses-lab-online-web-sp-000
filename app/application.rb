c53f6b401a45799212f882fc64660a633d391fee
zchesny <z.chesny@gmail.com>
2020-04-30 15:31:26 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,14 @@ class Application
       time = Time.now.hour
   
       resp.write "Time: #{time}\n"
+
+      resp.write time < 12 ? "Good Morning!" : "Good Afternoon!"
    
-      if time < 12
-        resp.write "Good Morning!"
-      else
-        resp.write "Good Afternoon!"
-      end
+    #   if time < 12
+    #     resp.write "Good Morning!"
+    #   else
+    #     resp.write "Good Afternoon!"
+    #   end
   
       resp.finish
     end
